319096415246a3ebf2937b2b3d0d05e0f99e486e
Andrehiu <hiuandre@gmail.com>
2017-02-01 11:38:46 +0800

    Add files via upload

---
+++ Fly.cpp
@@ -0,0 +1,34 @@
+#include "Fly.h"
+Fly::Fly() : Entity()
+{
+	spriteData.width = FlyNS::WIDTH;           // size of asteroid
+	spriteData.height = FlyNS::HEIGHT;
+	spriteData.x = FlyNS::X;                   // location on screen
+	spriteData.y = FlyNS::Y;
+	spriteData.rect.bottom = FlyNS::HEIGHT;    // rectangle to select parts of an image
+	spriteData.rect.right = FlyNS::WIDTH;
+	velocity.x = 50;                             // velocity X
+	velocity.y = 50;                             // velocity Y
+	mass = FlyNS::MASS;
+	collisionType = entityNS::BOX;
+	spriteData.scale = 1;
+}
+//=============================================================================
+// Initialize the Asteroid.
+// Post: returns true if successful, false if failed
+//=============================================================================
+bool Fly::initialize(Game *gamePtr, int width, int height, int ncols,
+	TextureManager *textureM)
+{
+	return(Entity::initialize(gamePtr, width, height, ncols, textureM));
+}
+void Fly::draw()
+{
+	Image::draw();
+}
+void Fly::update(float frameTime)
+{
+	Entity::update(frameTime);
+	spriteData.x -= frameTime * velocity.x;         // move ship along X
+	spriteData.y += (sin(spriteData.x / 10) * 5) / 5;
+}
---
+++ Fly.h
@@ -0,0 +1,33 @@
+#ifndef _FLY_H
+#define _FLY_H
+#define WIN32_LEAN_AND_MEAN
+#include "entity.h"
+#include "constants.h"
+#include <math.h>
+namespace FlyNS
+{
+	const int WIDTH = 100;
+	const int HEIGHT = 100;
+	const int X = 0;   // location on screen
+	const int Y = 0;
+	const float ROTATION_RATE = 0; // radians per second
+	const float SPEED = 0;                  // 0 pixels per second
+	const float MASS = 100.0f;              // mass
+
+}
+class Fly : public Entity
+{
+private:
+	boolean active = true;
+public:
+	// constructor
+	Fly();
+	// inherited member functions
+	virtual void draw();
+	virtual bool initialize(Game *gamePtr, int width, int height, int ncols,
+		TextureManager *textureM);
+	void update(float frameTime);
+	boolean getActive();
+	void setActive(boolean flag);
+};
+#endif // !_SPITBALL_H
